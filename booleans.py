f3f149dbf8d5c6319d30ab7626f785ddc25e284a
Kanagat <sauranbaykanagat@gmail.com>
2025-01-23 15:12:58 +0500

    bool,list,operator

---
+++ booleans.py
@@ -0,0 +1,38 @@
+#_______________________________________________
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+#_______________________________________________
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+#_______________________________________________
+print(bool("Hello"))
+print(bool(15))
+#_______________________________________________
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))  
+#_______________________________________________
+def myFunction() :
+  return True
+
+print(myFunction())
+#_______________________________________________
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+#_______________________________________________
+x = 200
+print(isinstance(x, int))
+#_______________________________________________
